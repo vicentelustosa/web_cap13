a52eb263a7e74322fd04e285a2f14b9c6afe78a8
Vicente Pires Lustosa Neto <vicente.pires@escolar.ifrn.edu.br>
2026-04-13 16:09:05 -0300

    correções

--- app/forms/user_form.py
+++ app/forms/user_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired
 
 class UserForm(FlaskForm):
     nome = StringField("Nome", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
